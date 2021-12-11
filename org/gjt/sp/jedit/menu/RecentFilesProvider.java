0a9b89781716b8527ee855c1707bb75f60359ce3
Uma <79767010+yams-yams@users.noreply.github.com>
2021-12-10 19:36:42 -0600

    Completed Change Request #2

--- org/gjt/sp/jedit/menu/RecentFilesProvider.java
+++ org/gjt/sp/jedit/menu/RecentFilesProvider.java
@@ -101,7 +101,7 @@ public class RecentFilesProvider implements DynamicMenuProvider
 					if ((! typedText.contains("*")) && (! typedText.contains("?")))
 					{
 						// Old style (before jEdit 4.3pre18): Match start of file name
-						regex += "*";
+						regex = "*" + regex + "*";
 					}
 					pattern = Pattern.compile(StandardUtilities.globToRE(regex),
 						Pattern.CASE_INSENSITIVE);
